1e3513335594a42ceaf2595bdb1be0c30196addc
ayman22205101566-art <ayman22205101566@diu.edu.bd>
2026-04-01 21:55:32 +0600

    build for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/B13-A6-DigiTools-Platform/',
-  plugins: [react() ,tailwindcss()],
+  base: '/',
+  plugins: [react(), tailwindcss()],
 })
